c2e7aab31f41ac48b18376a1b7ae7871f36c501f
Abbush <glgberat@yahoo.com>
2022-03-06 01:59:50 -0500

    Express middleware added

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+//Dependencies initilized
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3002; //This environment variable needed to deploy for Heroku
 const app = express();
 
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 
 
